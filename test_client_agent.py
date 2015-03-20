1ed4a1b2ed92a64c484fceb8f42a453df7ccba11
ephemeral2eternity <chenw@andrew.cmu.edu>
2015-03-20 00:45:09 +0000

    change qoe to the last one

--- test_client_agent.py
+++ test_client_agent.py
@@ -35,11 +35,6 @@ for i in range(1):
 	vidNum = 1000
 	video_id = weighted_choice(zipf_cdf)
 
-	## Testing QoE based server selection
-	method = 'qoe'
-	waitRandom(1, 100)
-	client_agent(cache_agent, video_id, method)
-
 	## Testing load based server selection
 	method = 'load'
 	waitRandom(1, 100)
@@ -59,4 +54,10 @@ for i in range(1):
 	method = 'random'
 	waitRandom(1, 100)
 	client_agent(cache_agent, video_id, method)
+	
+	## Testing QoE based server selection
+	method = 'qoe'
+	waitRandom(1, 100)
+	client_agent(cache_agent, video_id, method)
+
 
