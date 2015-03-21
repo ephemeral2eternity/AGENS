c1c96b6fd4b2152fe2ff56bb06ab83898d1d09c8
ephemeral2eternity <chenw@andrew.cmu.edu>
2015-03-21 17:48:12 +0000

    uncomment other method

--- test_client_agent.py
+++ test_client_agent.py
@@ -35,7 +35,6 @@ for i in range(1):
 	vidNum = 1000
 	video_id = weighted_choice(zipf_cdf)
 
-	'''
 	## Testing load based server selection
 	method = 'load'
 	waitRandom(1, 100)
@@ -55,7 +54,6 @@ for i in range(1):
 	method = 'random'
 	waitRandom(1, 100)
 	client_agent(cache_agent, video_id, method)
-	'''
 
 	## Testing QoE based server selection
 	method = 'qoe'
